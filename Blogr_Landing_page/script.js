deda20de5f9ce88fc6f2f6ceea8f07cad8db2484
Ash6402 <104356067+Ash6402@users.noreply.github.com>
2022-08-02 00:31:48 +0500

    Revert "Update script.js"

--- Blogr_Landing_page/script.js
+++ Blogr_Landing_page/script.js
@@ -10,6 +10,8 @@ menuToggle.addEventListener('click', ()=>{
 
 dropper.forEach((btn)=>{
     btn.addEventListener('click', ()=>{
+        // btn.style.setProperty("--rotate", "rotate(180deg)");
+        // btn.nextElementSibling.style.display = 'block';
 
         btn.classList.toggle('done');
         btn.nextElementSibling.classList.toggle('show');
@@ -17,12 +19,19 @@ dropper.forEach((btn)=>{
 
         dropper.forEach((btn2)=>{
             if(btn2 != btn){
+                // btn2.style.setProperty("--rotate", "rotate(0deg)");
+                // btn2.nextElementSibling.style.display = 'none';
 
                 btn2.classList.remove('done');
                 btn2.nextElementSibling.classList.remove('show');
             }
         })
     })
+
+    // btn.addEventListener('blur', ()=>{
+    //     btn.classList.remove('done');
+    //     btn.nextElementSibling.classList.remove('show');
+    // })
 })
 
 
