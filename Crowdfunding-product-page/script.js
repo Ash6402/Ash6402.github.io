e098bb2858bdfae2284b38290871960a7d84542d
Ash6402 <aashir.razzaq10@gmail.com>
2022-08-21 15:10:07 +0500

    Update script.js

--- Crowdfunding-product-page/script.js
+++ Crowdfunding-product-page/script.js
@@ -1,30 +1,112 @@
-let btn = document.querySelector('.menu-btn');
-let navList = document.querySelector('.nav-list');
-let bookmarkBtn = document.querySelector('#bookmark-btn');
-let bookmarkIcon = document.querySelector('.bookmark-icon');
-let headingBtn = document.querySelectorAll('.heading-btn');
-let modalClose = document.querySelector('.modal-closeBtn');
-let modalBacker = document.querySelector('.backer-modal');
-let radioInputs = document.querySelectorAll('input[type="radio"]');
-let backerBtn = document.querySelector("#backerBtn");
-let bambooStandBtn = document.querySelector('#bambooStand-Btn')
-let besBtn = document.querySelector("#BES-Btn");
-let numberInputs = document.querySelectorAll('.number');
-let success = document.querySelector("#success-modal-overlay");
-let successClose = document.querySelector("#successModal-Close");
+let menuBtn = document.querySelector('.menu-btn'); //--For Nav / Dropdown-menu--
+let navList = document.querySelector('.nav-list'); //--For Nav / Dropdown-menu--
 
-btn.addEventListener('click', ()=>{
+let bookmarkBtn = document.querySelector('#bookmark-btn'); //--For Bookmark--
+let bookmarkIcon = document.querySelector('.bookmark-icon'); //--For Boomark--
 
+let backerBtn = document.querySelector("#backerBtn"); //--To open Backer-menu--
+let modalBacker = document.querySelector('.backer-modal'); //--Backer-Modal--
+let modalClose = document.querySelector('.modal-closeBtn'); //--To Close Backer-menu--
+
+let headingBtns = document.querySelectorAll('.heading-btn'); //--For Backing-selection--
+let radioInputs = document.querySelectorAll('input[type="radio"]'); //--seclection using radio-inputs--
+
+let bambooStandBtn = document.querySelector('#bambooStand-Btn'); //--Backing by BambooStand--
+let besBtn = document.querySelector("#BES-Btn"); //--Backing by Special Edition Stand--
+let mseBtn = document.querySelector("#MSE-Btn");
+
+let forms = document.querySelectorAll(".pledge-amount"); //--Input Management--
+let numberInputs = document.querySelectorAll('.number'); //--Input Management--
+
+let success = document.querySelector("#success-modal-overlay"); //--For Success Modal--
+let successClose = document.querySelector("#successModal-Close"); //--For Success Modal--
+
+let backedAmount = document.querySelector("#backer-amount"); //--For State Management--
+let backersAmount = document.querySelector("#backers"); //--For State Management--
+let daysLeft = document.querySelector("#days-left"); //--For State Management--
+let stockAmount = document.querySelectorAll(".stock-amount"); //--To manage stocks--
+let statusBar = document.querySelector(".status"); //--For updating status bar--
+
+let bambooStand = document.querySelector("#bambooStand"); //--For Out-of-Stock Management--
+let bambooStandModal = document.querySelector("#bambooStandModal"); //--For Out-of-Stock Management--
+let besStand = document.querySelector("#besStand"); //--For Out-of-Stock Management--
+let besStandModal = document.querySelector("#besStandModal"); //--For Out-of-Stock Management--
+let mseStand = document.querySelector("#mseStand"); //--For Out-of-Stock Management--
+let mseStandModal = document.querySelector("#mseStandModal"); //--For Out-of-Stock Management--
+
+//------------------------------------------------------------------------
+//------------------State Management on page opening----------------------
+//------------------------------------------------------------------------
+
+
+backedAmount.innerHTML = localStorage.getItem('totalAmount');
+backersAmount.innerHTML = localStorage.getItem('backers');
+statusBar.style.width = (localStorage.getItem('status')) + '%';
+
+let timeLeft = setInterval(()=>{
+    let expiry = new Date(2022, 10, 1).getTime();
+    let current = new Date().getTime();
+    left = expiry - current;
+    days = Math.floor(left / (1000 * 60 * 60 * 24));
+    daysLeft.innerHTML = `${days}`;
+}, 1000)
+
+stockAmount[0].innerHTML = localStorage.getItem('stock1');
+stockAmount[3].innerHTML = localStorage.getItem('stock1');
+stockAmount[1].innerHTML = localStorage.getItem('stock2');
+stockAmount[4].innerHTML = localStorage.getItem('stock2');
+stockAmount[2].innerHTML = localStorage.getItem('stock3');
+stockAmount[5].innerHTML = localStorage.getItem('stock3');
+
+
+let stock1 = Number(localStorage.getItem('stock1'));
+let stock2 = Number(localStorage.getItem('stock2'));
+let stock3 = Number(localStorage.getItem('stock3'));
+
+if( stock1 == 0){
+   outofStock(bambooStand, bambooStandModal, forms[1]);
+}
+
+if(stock2 == 0){
+    outofStock(besStand, besStandModal, forms[2]);
+}
+
+if(stock3 == 0){
+    outofStock(mseStand, mseStandModal, forms[3]);
+}
+
+function outofStock(e1, e2, form){
+    e1.classList.add('out-of-stock');
+    e2.classList.add('out-of-stock');
+    e1.lastElementChild.lastElementChild.setAttribute('disabled', null);
+    e1.lastElementChild.lastElementChild.innerHTML = `Out of Stock`;
+    form.children[0].setAttribute('disabled', null);
+    form.children[1].setAttribute('disabled', null);
+    form.children[1].innerHTML = `Out of stock`;
+}
+
+//------------------------------------------------------------------------
+//-------------To Open and Close the Dropdown Menu------------------------
+//------------------------------------------------------------------------
+
+
+menuBtn.addEventListener('click', ()=>{
+    
     navList.classList.toggle('show');
-
+    
     if(navList.classList.contains('show')){
-        btn.setAttribute('src', './images/icon-close-menu.svg');
-
+        menuBtn.setAttribute('src', './images/icon-close-menu.svg');
+        
     }else{
-        btn.setAttribute('src', './images/icon-hamburger.svg');
+        menuBtn.setAttribute('src', './images/icon-hamburger.svg');
     }
-
+    
 })
+
+//------------------------------------------------------------------------
+//-----------------------For the Bookmark Button--------------------------
+//------------------------------------------------------------------------
+
 
 bookmarkBtn.addEventListener('click', ()=>{
     bookmarkIcon.classList.toggle('bookmarked');
@@ -32,23 +114,28 @@ bookmarkBtn.addEventListener('click', ()=>{
     span.classList.toggle('hide');
 })
 
+//-------------------------------------------------------------------------
+//-------------------Backer modal opening and closing-----------------------
+//-------------------------------------------------------------------------
+
+
 backerBtn.addEventListener('click', ()=>{
     modalBacker.setAttribute('open', null);
     modalBacker.scrollIntoView();
 })
 
-bambooStandBtn.addEventListener('click', ()=>{
-    RewardSelection(radioInputs[1]);
-    unCheck(radioInputs[0]);
-    unCheck(radioInputs[2]);
+modalClose.addEventListener('click', ()=>{
+    modalBacker.removeAttribute('open');
+        radioInputs.forEach((e)=>{
+        e.parentElement.parentElement.classList.remove('selected');
+        e.checked = false;
+        e.parentElement.parentElement.lastElementChild.classList.add('hide');
+    })
 })
 
-besBtn.addEventListener('click', ()=>{
-    RewardSelection(radioInputs[2]);
-    unCheck(radioInputs[0]);
-    unCheck(radioInputs[1]);
-})
-
+//-------------------------------------------------------------------------
+//--------------------Backer Modal functionality---------------------------
+//-------------------------------------------------------------------------
 
 
 radioInputs.forEach((e)=>{
@@ -63,62 +150,46 @@ radioInputs.forEach((e)=>{
     })
 })
 
-headingBtn.forEach((btn)=>{
-
+headingBtns.forEach((btn)=>{
     btn.addEventListener('click', ()=>{
+        btn.parentElement.parentElement.parentElement.classList.add('selected');
+        btn.parentElement.previousElementSibling.checked = true;
+        btn.parentElement.parentElement.parentElement.lastElementChild.classList.remove('hide');
 
-        let radio = btn.parentElement.parentElement.previousElementSibling;
-        
-        radio.checked = true;
-        radio.parentElement.parentElement.classList.add('selected');
-        btn.parentElement.parentElement.parentElement.parentElement.lastElementChild.classList.remove('hide');
-
-        headingBtn.forEach((btn2)=>{
+        headingBtns.forEach((btn2)=>{
             if(btn2 != btn){
-                let radio2 = btn2.parentElement.parentElement.previousElementSibling;
-                radio2.parentElement.parentElement.classList.remove('selected');
-                radio2.checked = false;
-                btn2.parentElement.parentElement.parentElement.parentElement.lastElementChild.classList.add('hide');
+                btn2.parentElement.parentElement.parentElement.classList.remove('selected');
+                btn2.parentElement.previousElementSibling.checked = false;
+                btn2.parentElement.parentElement.parentElement.lastElementChild.classList.add('hide');
             }
         })
-
-    })
-
-})
-
-modalClose.addEventListener('click', ()=>{
-    modalBacker.removeAttribute('open');
-    radioInputs.forEach((e)=>{
-        e.parentElement.parentElement.classList.remove('selected');
-        e.checked = false;
-        e.parentElement.parentElement.lastElementChild.classList.add('hide');
     })
 })
 
-numberInputs.forEach(input=>{
-    if(input == numberInputs[0]){
-        input.addEventListener('keyup', ()=>{
-            if(input.value < 25){
-                input.style.outline = "1px solid red";
-            }else{
-                input.style.outline = "";
-                clearData(input);
-            }
-        })
-    }else if(input == numberInputs[1]){
-        input.addEventListener('keyup', ()=>{
-            if(input.value < 75){
-                input.style.outline = "1px solid red";
-            }else{
-                input.style.outline = "";
-                clearData(input);
-            }
-        })
-    }
+//-------------------------------------------------------------------------
+//-------------------Reward Selection Button Functionaliy------------------
+//-------------------------------------------------------------------------
+
+
+bambooStandBtn.addEventListener('click', ()=>{
+    RewardSelection(radioInputs[1]);
+    unCheck(radioInputs[0]);
+    unCheck(radioInputs[2]);
+    unCheck(radioInputs[3]);
 })
 
-successClose.addEventListener('click', ()=>{
-    success.classList.remove('show-modal');
+besBtn.addEventListener('click', ()=>{
+    RewardSelection(radioInputs[2]);
+    unCheck(radioInputs[0]);
+    unCheck(radioInputs[1]);
+    unCheck(radioInputs[3]);
+})
+
+mseBtn.addEventListener('click', ()=>{
+    RewardSelection(radioInputs[3]);
+    unCheck(radioInputs[0]);
+    unCheck(radioInputs[1]);
+    unCheck(radioInputs[2]);
 })
 
 let RewardSelection = input =>{
@@ -135,17 +206,100 @@ let unCheck = selector =>{
     selector.parentElement.parentElement.lastElementChild.classList.add('hide');
 }
 
-let clearData = data =>{
-    data.nextElementSibling.addEventListener('click', ()=>{
-        success.classList.add('show-modal');
-        modalBacker.removeAttribute('open');
-        radioInputs.forEach((e)=>{
-            e.parentElement.parentElement.classList.remove('selected');
-            e.checked = false;
-            e.parentElement.parentElement.lastElementChild.classList.add('hide');
-        })
-        numberInputs.forEach((e)=>{
-            e.value = "";
-        })
+//-------------------------------------------------------------------------
+//------------------For Opening and Closing Success Modal------------------
+//-------------------------------------------------------------------------
+
+
+let successModal = () =>{
+    success.classList.add('show-modal');
+
+    modalBacker.removeAttribute('open');
+    radioInputs.forEach((e)=>{
+        e.parentElement.parentElement.classList.remove('selected');
+        e.checked = false;
+        e.parentElement.parentElement.lastElementChild.classList.add('hide');
     })
 }
+
+
+successClose.addEventListener('click', ()=>{
+    success.classList.remove('show-modal');
+})
+
+//-------------------------------------------------------------------------
+// ------------------------ Form Management -------------------------------
+//-------------------------------------------------------------------------
+
+forms[0].addEventListener('submit', ()=>{
+    successModal();
+})
+
+forms[1].addEventListener('submit', (e)=>{
+    forms_and_StateManagement(e ,forms[1], 25, 'stock1', stockAmount[0], stockAmount[3]);
+     
+})
+
+forms[2].addEventListener('submit', (e)=>{
+    forms_and_StateManagement(e, forms[2], 75, 'stock2', stockAmount[1], stockAmount[4]);
+})
+
+forms[3].addEventListener('submit', (e)=>{
+    forms_and_StateManagement(e, forms[3], 200, 'stock3', stockAmount[2], stockAmount[5]);
+})
+
+let forms_and_StateManagement = (eventObject ,form, leastAmount, stock, display1, display2) =>{
+
+    let input = form.children[0];
+
+    if(input.value < leastAmount){
+        input.classList.add('invalid');
+        eventObject.preventDefault();
+    }else{
+
+        successModal();
+
+        let remainingStock = Number(localStorage.getItem(stock));
+        remainingStock += -1;
+        display1.innerHTML = remainingStock;
+        display2.innerHTML = remainingStock;
+        localStorage.setItem(stock, remainingStock);
+
+        backers = Number(localStorage.getItem('backers'));
+        ++backers;
+        backersAmount.innerHTML = backers;
+        localStorage.setItem('backers', backers);
+
+        let totalAmount = Number(localStorage.getItem('totalAmount'));
+        totalAmount += Number(input.value);
+        backedAmount.innerHTML = `$${totalAmount}`;
+        localStorage.setItem('totalAmount', totalAmount);
+
+        let currentStatus = Number(localStorage.getItem('status')); 
+        currentStatus += (totalAmount/100000)*100;
+        statusBar.style.width = currentStatus+`%`;
+        localStorage.setItem('status', currentStatus);
+
+        if( stock == `stock1` &&  remainingStock == 0){
+            outofStock(bambooStand, bambooStandModal, forms[1]);
+        }
+         
+        if(stock == `stock2` &&  remainingStock == 0){
+            outofStock(besStand, besStandModal, forms[2]);
+        }
+         
+        if(stock == `stock3` &&  remainingStock == 0){
+            outofStock(mseStand, mseStandModal, forms[3]);
+        }
+
+        input.value = null;
+    }
+
+}
+
+// localStorage.setItem('totalAmount', 0);
+// localStorage.setItem('stock1', 300);
+// localStorage.setItem('stock2', 300);
+// localStorage.setItem('stock3', 300);
+// localStorage.setItem('backers', 0);
+// localStorage.setItem('status', 0);
