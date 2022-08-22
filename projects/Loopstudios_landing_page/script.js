2f3256b08ffaaefbbbe9c6e8af64dca36376a14a
Ash6402 <aashir.razzaq10@gmail.com>
2022-08-22 22:07:36 +0500

    Update script.js

--- projects/Loopstudios_landing_page/script.js
+++ projects/Loopstudios_landing_page/script.js
@@ -5,12 +5,14 @@ let menuBtnImg = document.querySelector('#menuBtn-image');
 
 menuBtn.addEventListener('click', ()=>{
 
-    navList.classList.toggle('showList')
+    navList.classList.toggle('showList');
     
     if(navList.classList.contains('showList')){
         menuBtnImg.setAttribute('src', './images/icon-close.svg');
+        navList.classList.remove('hideList');
     }else{
         menuBtnImg.setAttribute('src', './images/icon-hamburger.svg');
+        navList.classList.add('hideList');
     }
 
     menuBtn.classList.toggle('fixed');
